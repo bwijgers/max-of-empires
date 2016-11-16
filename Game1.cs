b57497214cd6244d0306ba71ad4214b7d8af1b32
OmeLuuk <Luuk@sterke.net>
2016-11-16 14:51:06 +0100

    Change background color
    Background color is now white

--- Game1.cs
+++ Game1.cs
@@ -71,7 +71,7 @@ namespace MaxOfEmpires
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.White);
 
             // TODO: Add your drawing code here
 
