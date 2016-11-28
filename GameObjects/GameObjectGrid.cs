3249ec7c0d8c33f3001e0ac47ba39cbc3c0b8d8b
Ebilkill <bartwijgers@hotmail.com>
2016-11-28 12:30:56 +0100

    Create GameObjectXXX classes
    GameObject:
      - Head of the classes
      - Abstract
      - Defines all functions every object in the game needs
    
    GameObjectList/GameObjectGrid allows users to set GameObjects in the list/grid and passes all virtual methods of GameObject to the GameObjects in the list/grid.

---
+++ GameObjects/GameObjectGrid.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MaxOfEmpires.GameObjects
+{
+    /// <summary>
+    /// A grid of GameObjects. Similar to GameObjectList, except that the orientation of the objects matters in this case.
+    /// </summary>
+    /// <see cref="GameObjectList"/>
+    class GameObjectGrid : GameObject
+    {
+        /// <summary>
+        /// Used by this class for methods that need to be called on every element.
+        /// </summary>
+        /// <param name="obj">The GameObject the method is called upon.</param>
+        /// <param name="x">The x-position in the grid of this GameObject.</param>
+        /// <param name="y">The y-position in the grid of this GameObject.</param>
+        private delegate void GridFunc(GameObject obj, int x, int y);
+
+        /// <summary>
+        /// The grid. A digital-- Wait, I did this already.
+        /// </summary>
+        private GameObject[,] grid;
+
+        /// <summary>
+        /// Creates a new GameObjectGrid.
+        /// </summary>
+        /// <param name="width">The width of the grid.</param>
+        /// <param name="height">The height of the grid.</param>
+        /// <param name="id">The name of the grid. Empty by default.</param>
+        public GameObjectGrid(int width, int height, string id = "") : base (id)
+        {
+            grid = new GameObject[width, height];
+        }
+
+        /// <see cref="GameObject.Draw(GameTime, SpriteBatch)"/>
+        public override void Draw(GameTime time, SpriteBatch s)
+        {
+            // Draw every element on the grid if it is not null.
+            ForEach((obj, x, y) => obj?.Draw(time, s));
+        }
+
+        /// <summary>
+        /// Calles a function for every element in the grid.
+        /// </summary>
+        /// <param name="func">The function to call for each element.</param>
+        private void ForEach(GridFunc func)
+        {
+            // Make sure we gotta do something before we try doing something.
+            if (func == null || func.GetInvocationList().Length == 0)
+                return;
+
+            // Do whatever is desired for every element in this grid.
+            for (int x = 0; x < Width; ++x)
+            {
+                for (int y = 0; y < Height; ++y)
+                {
+                    func.Invoke(grid[x, y], x, y);
+                }
+            }
+        }
+
+        /// <see cref="GameObject.HandleInput(InputHelper, KeyManager)"/>
+        public override void HandleInput(InputHelper helper, KeyManager keyManager)
+        {
+            // Handle input for every element in grid
+            ForEach((obj, x, y) => obj?.HandleInput(helper, keyManager));
+        }
+
+        /// <summary>
+        /// Gets the object at the specified position, or null if there is no such object.
+        /// </summary>
+        /// <param name="x">The X-position to check at.</param>
+        /// <param name="y">The y-position to check at.</param>
+        /// <returns>The GameObject at the specified position, which can be null, or null if the position was out of the grid bounds.</returns>
+        public GameObject ObjectAt(int x, int y)
+        {
+            // If the coords are in bounds, return the object at the coords.
+            if (x >= 0 && y >= 0 && x < Width && y < Height)
+                return grid[x, y];
+
+            // Otherwise, return null
+            return null;
+        }
+
+        /// <see cref="GameObject.Reset"/>
+        public override void Reset()
+        {
+            // Reset every element in the grid
+            ForEach((obj, x, y) => obj?.Reset());
+        }
+
+        /// <summary>
+        /// Sets an element in the grid. Returns a success code.
+        /// </summary>
+        /// <param name="obj">The object to place in the grid.</param>
+        /// <param name="x">The x-position to place the object in.</param>
+        /// <param name="y">The y-position to place the object in.</param>
+        /// <param name="force">Whether to force placement; if true, ignores whatever is in the position already and overwrites it.</param>
+        /// <returns>True if this object was placed at the specified position, false otherwise.</returns>
+        public bool SetElementInGrid(GameObject obj, int x, int y, bool force = false)
+        {
+            // If we can't place this object here, return false
+            if (!force && grid[x, y] != null)
+                return false;
+
+            // Place the object here and return true
+            grid[x, y] = obj;
+            return true;
+        }
+
+        /// <see cref="GameObject.Update(GameTime)"/>
+        public override void Update(GameTime time)
+        {
+            // Just update everything in the grid
+            ForEach((obj, x, y) => obj?.Update(time));
+        }
+
+        /// <summary>
+        /// The width of this grid, being the amount of elements in the x-direction.
+        /// </summary>
+        public int Width => grid.GetLength(0);
+
+        /// <summary>
+        /// The height of this grid, being the amount of elements in the y-direction.
+        /// </summary>
+        public int Height => grid.GetLength(1);
+    }
+}
---
+++ GameObjects/GameObjectList.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MaxOfEmpires.GameObjects
+{
+    /// <summary>
+    /// A list of GameObjects. This class is meant to group different GameObjects together if they don't belong in a specific orientation or order.
+    /// </summary>
+    class GameObjectList : GameObject
+    {
+        /// <summary>
+        /// A function that can be called on every element in this list. Used in conjunction with the ForEach method.
+        /// </summary>
+        /// <param name="obj">The current GameObject in the iteration of all elements of this list.</param>
+        /// <see cref="ForEach(ListFunc)"/>
+        private delegate void ListFunc(GameObject obj);
+
+        /// <summary>
+        /// The list of all GameObjects contained by this list.
+        /// </summary>
+        private List<GameObject> children;
+
+        /// <summary>
+        /// Creates a new GameObjectList.
+        /// </summary>
+        public GameObjectList(string id = "") : base(id)
+        {
+            children = new List<GameObject>();
+        }
+
+        /// <summary>
+        /// Adds a GameObject to this GameObjectList. Also sets the GameObject's parent equal to this list.
+        /// </summary>
+        /// <param name="obj">The GameObject to add to this list.</param>
+        public void Add(GameObject obj)
+        {
+            children.Add(obj);
+            obj.Parent = this;
+        }
+
+        public override void Draw(GameTime time, SpriteBatch s)
+        {
+            // Drawing a list just means drawing its children
+            ForEach(child => child?.Draw(time, s));
+        }
+
+        private void ForEach(ListFunc func)
+        {
+            for (int i = children.Count - 1; i >= 0; --i)
+            {
+                func.Invoke(children[i]);
+            }
+        }
+
+        public override void HandleInput(InputHelper helper, KeyManager keyManager)
+        {
+            // Managing a list's input is pretty easy; just let everyone else handle theirs.
+            ForEach(child => child?.HandleInput(helper, keyManager));
+        }
+
+        /// <summary>
+        /// Removes a GameObject from this list. Does not search inner lists to remove the object from them; only elements contained in THIS list can be removed this way.
+        /// </summary>
+        /// <param name="obj">The GameObject to remove from this list.</param>
+        public void RemoveChild(GameObject obj)
+        {
+            // Check if this list has the object
+            if (children.Contains(obj))
+            {
+                // Remove it
+                children.Remove(obj);
+            }
+        }
+
+        public override void Reset()
+        {
+            // Resetting this list means resetting all of its kids
+            ForEach(child => child?.Reset());
+        }
+
+        public override void Update(GameTime time)
+        {
+            // Updating this list means just update all children.
+            ForEach(child => child?.Update(time));
+        }
+    }
+}
