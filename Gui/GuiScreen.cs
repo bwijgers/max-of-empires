381f4f4ad7fe5255be22ac32f6b740925d6a1f47
Ebilkill <bartwijgers@hotmail.com>
2016-12-21 10:20:42 +0100

    Update Gui lib
    After a click, all other click events are ignored

--- Gui/GuiScreen.cs
+++ Gui/GuiScreen.cs
@@ -77,6 +77,9 @@ namespace Ebilkill.Gui
 
                         // Make this element active
                         activeElement = i;
+
+                        // An element has been clicked, check no more
+                        break;
                     }
                 }
             }
