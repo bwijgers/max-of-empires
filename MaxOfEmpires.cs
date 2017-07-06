514809f7a4ba1b9bd02fcc0209404acec590bab4
Ebilkill <bartwijgers@hotmail.com>
2017-07-06 13:57:21 +0200

    Change the player colors slightly :)

--- MaxOfEmpires.cs
+++ MaxOfEmpires.cs
@@ -131,8 +131,8 @@ namespace MaxOfEmpires
             LoadConfiguration();
 
             // Load players
-            Player blue = new Player("Blue", "Blue", Color.CornflowerBlue, mainConfiguration.GetProperty<int>("player.startingMoney"));
-            Player red = new Player("Red", "Red", Color.Red, mainConfiguration.GetProperty<int>("player.startingMoney"));
+            Player blue = new Player("Blue", "Blue", Color.SteelBlue, mainConfiguration.GetProperty<int>("player.startingMoney"));
+            Player red = new Player("Red", "Red", Color.IndianRed, mainConfiguration.GetProperty<int>("player.startingMoney"));
 
             // Adds battleState to the GamestateManager
             GameStateManager.AddState("economy", new EconomyState(blue, red));
